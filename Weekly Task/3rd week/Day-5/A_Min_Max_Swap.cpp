0d0486d863ba1516304996f1699d7e15e995461e
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-01-21 22:03:27 +0600

    Day-5

---
+++ Weekly Task/3rd week/Day-5/A_Min_Max_Swap.cpp
@@ -0,0 +1,63 @@
+
+/**************************************************************
+**               BISMILLAH HIR RAHMAN NIR RAHIM              **
+**             https://github.com/Md-Merazul-Islam           **
+**************************************************************/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define PI 3.14159265359
+#define dmin 1e-9
+#define dd double
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define vi vector<ll>
+#define vpi vector<pair<ll, ll>>
+#define pii pair<ll, ll>
+#define srt(a) sort(a.begin(), a.end())
+#define all(a) a.begin(), a.end()
+#define rsrt(a) sort(a.rbegin(), a.rend())
+#define line "\n"
+#define nl cout << "\n"
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define loop(i, a, b) for (int i = (a); i < (b); ++i)
+#define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
+void print(vector<ll> &a) { loop(i, 0, a.size()) cout << a[i] << ' '; }
+template <typename T>
+using my_ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int N = 2e5 + 5;
+
+void hello_wrold_solve_here()
+{
+
+    int n;
+    cin >> n;
+    vi a(n), b(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > b[i])
+            swap(a[i], b[i]);
+    }
+
+    int tmp = *max_element(all(a));
+    int tmp2 = *max_element(all(b));
+    cout << tmp * tmp2 << endl;
+}
+
+int32_t main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        hello_wrold_solve_here();
+    return 0;
+}
---
+++ Weekly Task/3rd week/Day-5/B_Fun_with_Even_Subarrays.cpp
@@ -0,0 +1,63 @@
+
+/**************************************************************
+**               BISMILLAH HIR RAHMAN NIR RAHIM              **
+**             https://github.com/Md-Merazul-Islam           **
+**************************************************************/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define PI 3.14159265359
+#define dmin 1e-9
+#define dd double
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define vi vector<ll>
+#define vpi vector<pair<ll, ll>>
+#define pii pair<ll, ll>
+#define srt(a) sort(a.begin(), a.end())
+#define rsrt(a) sort(a.rbegin(), a.rend())
+#define line "\n"
+#define nl cout << "\n"
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define loop(i, a, b) for (int i = (a); i < (b); ++i)
+#define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
+void print(vector<ll> &a) { loop(i, 0, a.size()) cout << a[i] << ' '; }
+template <typename T>
+using my_ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int N = 2e5 + 5;
+void hello_wrold_solve_here()
+{
+    int n;
+    cin >> n;
+    vi a(n+1);
+    for(int i=1;i<=n;i++) cin >> a[i];
+    ll l = n, cnt = 0;
+    while (l > 1)
+    {
+        if (a[l - 1] == a[n])
+        {
+            l--;
+            
+        }
+        else
+        {
+            l -= n - l + 1 ;
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+}
+
+int32_t main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        hello_wrold_solve_here();
+    return 0;
+}
