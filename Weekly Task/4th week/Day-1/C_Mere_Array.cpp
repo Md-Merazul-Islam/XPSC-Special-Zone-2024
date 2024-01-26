5a64a106cae5d959c2428fb30e48a7afef10aacb
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-01-26 21:55:20 +0600

    Dya

---
+++ Weekly Task/4th week/Day-1/C_Mere_Array.cpp
@@ -0,0 +1,65 @@
+
+/**************************************************************
+**               BISMILLAH HIR RAHMAN NIR RAHIM              **
+**             https://github.com/Md-Merazul-Islam           **
+**************************************************************/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define PI 3.14159265359
+#define dmin 1e-9
+#define dd double
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define vi vector<ll>
+#define vpi vector<pair<ll, ll>>
+#define pii pair<ll, ll>
+#define srt(a) sort(a.begin(), a.end())
+#define all(a) a.begin(), a.end()
+#define rsrt(a) sort(a.rbegin(), a.rend())
+#define line "\n"
+#define nl cout << "\n"
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define loop(i, a, b) for (int i = (a); i < (b); ++i)
+#define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
+#define scan(a) loop(i, 0, a.size()) cin >> a[i]
+void print(vector<ll> &a) { loop(i, 0, a.size()) cout << a[i] << ' '; }
+template <typename T>
+using my_ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int N = 2e5 + 5;
+void hello_wrold_solve_here()
+{
+
+    ll n;
+    cin >> n;
+    vi a(n);
+    scan(a);
+    ll x = *min_element(all(a));
+    vi ans(n);
+    for (int i = 0; i < n; i++)
+    {
+        ans[i] = a[i];
+    }
+    srt(a);
+    for (int i=0; i<n; i++)
+    {
+        if(ans[i]!=a[i] && a[i]%x!=0){
+            no;return;
+        }
+    }
+    yes;
+}
+
+signed main()
+{
+    fast;
+    int t = 1;
+    cin >> t;
+    while (t--)
+        hello_wrold_solve_here();
+    return 0;
+}
